033b54762e42e2d6b9c3543dc7617bec8d563050
Zach Baxter <zbax777@gmail.com>
2016-02-11 19:15:56 -0600

    Added some styles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160131165841) do
+ActiveRecord::Schema.define(version: 20160131225706) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -31,5 +31,12 @@ ActiveRecord::Schema.define(version: 20160131165841) do
   end
 
   add_index "recipes", ["category_id"], name: "index_recipes_on_category_id"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
 end
